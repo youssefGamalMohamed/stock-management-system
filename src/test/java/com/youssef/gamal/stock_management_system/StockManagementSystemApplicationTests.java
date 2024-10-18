4cddd074cdb5ff0fd6fe7f5f43ffd617d1f73e57
Youssef Gamal <Youssef.Gamal@intercom.com.eg>
2024-10-18 17:05:51 +0300

    chore: initialize project with required maven dependencies

---
+++ src/test/java/com/youssef/gamal/stock_management_system/StockManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youssef.gamal.stock_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
