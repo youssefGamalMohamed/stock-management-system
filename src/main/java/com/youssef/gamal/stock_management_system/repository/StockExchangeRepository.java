728dd6c63ee0d4fc68a1804176fcfdb1b7955802
Youssef Gamal <Youssef.Gamal@intercom.com.eg>
2024-10-18 17:12:04 +0300

    feat: adding jpa repositories for Stock and StockExahcnage

--- src/main/java/com/youssef/gamal/stock_management_system/repository/StockExchangeRepository.java
+++ src/main/java/com/youssef/gamal/stock_management_system/repository/StockExchangeRepository.java
@@ -1,2 +1,7 @@
-package com.youssef.gamal.stock_management_system.repository;public interface StockExchangeRepository {
+package com.youssef.gamal.stock_management_system.repository;
+
+import com.youssef.gamal.stock_management_system.entity.StockExchange;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StockExchangeRepository extends JpaRepository<StockExchange, Long> {
 }
--- src/main/java/com/youssef/gamal/stock_management_system/repository/StockRepository.java
+++ src/main/java/com/youssef/gamal/stock_management_system/repository/StockRepository.java
@@ -1,2 +1,7 @@
-package com.youssef.gamal.stock_management_system.repository;public interface StockRepository {
+package com.youssef.gamal.stock_management_system.repository;
+
+import com.youssef.gamal.stock_management_system.entity.Stock;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StockRepository extends JpaRepository<Stock, Long> {
 }
