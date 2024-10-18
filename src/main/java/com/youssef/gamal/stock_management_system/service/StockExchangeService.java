fe91be776b2fe0f642297206315dea424adfb26d
Youssef Gamal <Youssef.Gamal@intercom.com.eg>
2024-10-18 17:24:08 +0300

    feat: adding services implementations for Stock and StockExahcnage

--- src/main/java/com/youssef/gamal/stock_management_system/service/StockExchangeService.java
+++ src/main/java/com/youssef/gamal/stock_management_system/service/StockExchangeService.java
@@ -1,2 +1,50 @@
-package com.youssef.gamal.stock_management_system.service;public class StockExchangeService {
+package com.youssef.gamal.stock_management_system.service;
+
+import com.youssef.gamal.stock_management_system.entity.Stock;
+import com.youssef.gamal.stock_management_system.entity.StockExchange;
+import com.youssef.gamal.stock_management_system.repository.StockExchangeRepository;
+import com.youssef.gamal.stock_management_system.repository.StockRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class StockExchangeService {
+    @Autowired
+    private StockExchangeRepository stockExchangeRepository;
+    @Autowired
+    private StockRepository stockRepository;
+
+    public List<StockExchange> getAllExchanges() {
+        return stockExchangeRepository.findAll();
+    }
+
+    public StockExchange createStockExchange(StockExchange exchange) {
+        return stockExchangeRepository.save(exchange);
+    }
+
+    public void addStockToExchange(Long exchangeId, Long stockId) {
+        StockExchange exchange = stockExchangeRepository.findById(exchangeId).orElseThrow();
+        Stock stock = stockRepository.findById(stockId).orElseThrow();
+        exchange.getStocks().add(stock);
+        if (exchange.getStocks().size() >= 10) {
+            exchange.setLiveInMarket(true);
+        }
+        stockExchangeRepository.save(exchange);
+    }
+
+    public void removeStockFromExchange(Long exchangeId, Long stockId) {
+        StockExchange exchange = stockExchangeRepository.findById(exchangeId).orElseThrow();
+        Stock stock = stockRepository.findById(stockId).orElseThrow();
+        exchange.getStocks().remove(stock);
+        if (exchange.getStocks().size() < 10) {
+            exchange.setLiveInMarket(false);
+        }
+        stockExchangeRepository.save(exchange);
+    }
+
+    public void deleteStockExchange(Long exchangeId) {
+        stockExchangeRepository.deleteById(exchangeId);
+    }
 }
--- src/main/java/com/youssef/gamal/stock_management_system/service/StockService.java
+++ src/main/java/com/youssef/gamal/stock_management_system/service/StockService.java
@@ -1,2 +1,36 @@
-package com.youssef.gamal.stock_management_system.service;public class StockService {
+package com.youssef.gamal.stock_management_system.service;
+
+import com.youssef.gamal.stock_management_system.entity.Stock;
+import com.youssef.gamal.stock_management_system.repository.StockRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+import java.util.List;
+
+@Service
+public class StockService {
+    @Autowired
+    private StockRepository stockRepository;
+
+    public List<Stock> getAllStocks() {
+        return stockRepository.findAll();
+    }
+
+    public Stock createStock(Stock stock) {
+        stock.setLastUpdate(new Timestamp(System.currentTimeMillis()));
+        return stockRepository.save(stock);
+    }
+
+    public Stock updatePrice(Long stockId, BigDecimal newPrice) {
+        Stock stock = stockRepository.findById(stockId).orElseThrow();
+        stock.setCurrentPrice(newPrice);
+        stock.setLastUpdate(new Timestamp(System.currentTimeMillis()));
+        return stockRepository.save(stock);
+    }
+
+    public void deleteStock(Long stockId) {
+        stockRepository.deleteById(stockId);
+    }
 }
